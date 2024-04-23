dbfbb0ed2980fa844ecbd1ade60a4318e45b246d
maruf-ahmed <marufahamed1110@gmail.com>
2024-04-23 20:07:21 +0600

    Added delete functionality

--- index.js
+++ index.js
@@ -97,6 +97,13 @@ async function run() {
       const user = await cursor.toArray();
       res.send(user);
     });
+
+    app.delete("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
